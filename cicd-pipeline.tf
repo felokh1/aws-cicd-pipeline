59441d1dd4558e4b58e3a1b97e5e7a9e548838ad
felokh1 <94584977+felokh1@users.noreply.github.com>
2022-04-26 14:54:39 -0400

    5th commit

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -12,8 +12,8 @@ resource "aws_codebuild_project" "tf-plan" {
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     #image                      =  "hashicorp/terraform:latest" # The image is obtained from dockerhub (https://hub.docker.com/r/hashicorp/terraform/tags)
-    #image                       = "hashicorp/terraform:0.14.3"
-    image                      = "nginx"
+    image                       = "hashicorp/terraform:0.14.3"
+    #image                      = "nginx"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential{
@@ -41,8 +41,8 @@ resource "aws_codebuild_project" "tf-apply" {
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     #image                        = "hashicorp/terraform:latest" # The image is obtained from dockerhub (https://hub.docker.com/r/hashicorp/terraform/tags)
-    #image                       = "hashicorp/terraform:0.14.3"
-    image                       = "nginx"
+    image                       = "hashicorp/terraform:0.14.3"
+    #image                       = "nginx"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential{
